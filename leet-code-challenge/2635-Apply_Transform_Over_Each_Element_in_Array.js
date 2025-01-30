e68cf8a2d8a0c7be5fa6c064f60aa795e5cd1b90
rinshad101 <rinshadkk1111@gmail.com>
2025-01-30 10:50:14 +0530

    new

--- leet-code-challenge/2635-Apply_Transform_Over_Each_Element_in_Array.js
+++ leet-code-challenge/2635-Apply_Transform_Over_Each_Element_in_Array.js
@@ -4,14 +4,13 @@
 
 // Please solve it without the built-in Array.map method.
 
-
 // Example 1:
 
 // Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
 // Output: [2,3,4]
 // Explanation:
 // const newArray = map(arr, plusone); // [2,3,4]
-// The function increases each value in the array by one. 
+// The function increases each value in the array by one.
 // Example 2:
 
 // Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
@@ -22,3 +21,11 @@
 // Input: arr = [10,20,30], fn = function constant() { return 42; }
 // Output: [42,42,42]
 // Explanation: The function always returns 42.
+
+var map = function (arr, fn) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(fn(arr[i], i));
+  }
+  return newArr;
+};
