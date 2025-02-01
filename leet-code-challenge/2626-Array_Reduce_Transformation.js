86092e9fd7f88e19e47e39d60a1414c509b31a2a
rinshad101 <rinshadkk1111@gmail.com>
2025-02-01 10:03:02 +0530

    new

--- leet-code-challenge/2626-Array_Reduce_Transformation.js
+++ leet-code-challenge/2626-Array_Reduce_Transformation.js
@@ -44,3 +44,16 @@
 // init = 25
 // Output: 25
 // Explanation: For empty arrays, the answer is always init.
+
+
+var reduce = function (nums, fn, init) {
+    if (nums == []) {
+        return init;
+    } else {
+        let val = init;
+        for (let i = 0; i < nums.length; i++) {
+            val = fn(val, nums[i])
+        }
+        return val;
+    }
+};
