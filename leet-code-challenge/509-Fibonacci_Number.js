86fe0465d9a3da0eb85968095a836b434b174e67
rinshad101 <rinshadkk1111@gmail.com>
2025-02-10 09:58:52 +0530

    new

--- leet-code-challenge/509-Fibonacci_Number.js
+++ leet-code-challenge/509-Fibonacci_Number.js
@@ -4,8 +4,6 @@
 // F(n) = F(n - 1) + F(n - 2), for n > 1.
 // Given n, calculate F(n).
 
- 
-
 // Example 1:
 
 // Input: n = 2
@@ -21,3 +19,22 @@
 // Input: n = 4
 // Output: 3
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+var fib = function (n) {
+  if (n === 0) {
+    return 0;
+  }
+  let fib = [];
+  let n1 = 1,
+    n2 = 1,
+    nexTerm;
+  for (let i = 1; i <= n; i++) {
+    fib.push(n1);
+    nexTerm = n1 + n2;
+    n1 = n2;
+    n2 = nexTerm;
+  }
+  return fib[fib.length - 1];
+};
+
+console.log(fib(5))//5
