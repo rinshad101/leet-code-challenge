ef7624d8c533168d702d2d03a30ad165eb241740
rinshad101 <rinshadkk1111@gmail.com>
2025-02-17 10:41:06 +0530

    new

--- leet-code-challenge/21-Merge_Two_Sorted_Lists.js
+++ leet-code-challenge/21-Merge_Two_Sorted_Lists.js
@@ -18,3 +18,54 @@
 
 // Input: list1 = [], list2 = [0]
 // Output: [0]
+
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function mergeTwoLists(list1, list2) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    
+    current.next = list1 !== null ? list1 : list2;
+    
+    return dummyHead.next;
+}
+
+function arrayToLinkedList(arr) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    for (let num of arr) {
+        current.next = new ListNode(num);
+        current = current.next;
+    }
+    return dummyHead.next;
+}
+
+function linkedListToArray(node) {
+    let result = [];
+    while (node) {
+        result.push(node.val);
+        node = node.next;
+    }
+    return result;
+}
+
+let list1 = arrayToLinkedList([1,2,4]);
+let list2 = arrayToLinkedList([1,3,4]);
+let mergedList = mergeTwoLists(list1, list2);
+console.log(linkedListToArray(mergedList)); // Output: [1,1,2,3,4,4]
