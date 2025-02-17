00babc58b6c2c8662c5e0603c144208e3f1b4f71
rinshad101 <rinshadkk1111@gmail.com>
2025-02-17 10:35:40 +0530

    new

--- leet-code-challenge/2-Add_Two_Numbers.js
+++ leet-code-challenge/2-Add_Two_Numbers.js
@@ -20,3 +20,59 @@
 
 
 // using javascript
+
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let carry = 0;
+    
+    while (l1 !== null || l2 !== null) {
+        let x = l1 ? l1.val : 0;
+        let y = l2 ? l2.val : 0;
+        let sum = x + y + carry;
+        
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+    
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+    
+    return dummyHead.next;
+}
+
+function arrayToLinkedList(arr) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    for (let num of arr) {
+        current.next = new ListNode(num);
+        current = current.next;
+    }
+    return dummyHead.next;
+}
+
+function linkedListToArray(node) {
+    let result = [];
+    while (node) {
+        result.push(node.val);
+        node = node.next;
+    }
+    return result;
+}
+
+let l1 = arrayToLinkedList([2,4,3]);
+let l2 = arrayToLinkedList([5,6,4]);
+let sumList = addTwoNumbers(l1, l2);
+console.log(linkedListToArray(sumList)); // Output: [7,0,8]
