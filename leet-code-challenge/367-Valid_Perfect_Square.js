433876b4609b62b0db1f7b9baa1796aba6047c9e
rinshad101 <rinshadkk1111@gmail.com>
2025-02-20 09:30:12 +0530

    new

--- leet-code-challenge/367-Valid_Perfect_Square.js
+++ leet-code-challenge/367-Valid_Perfect_Square.js
@@ -18,3 +18,23 @@
 // Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
 
 
+var isPerfectSquare = function(num) {
+    if (num < 1) return false;
+    
+    let left = 1, right = num;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let square = mid * mid;
+
+        if (square === num) {
+            return true;
+        } else if (square < num) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+}   
+
+console.log(isPerfectSquare(16)) // true
