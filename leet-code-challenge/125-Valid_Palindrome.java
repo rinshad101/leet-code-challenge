dc1f4a61866ba813b3fc1604361a163e0d67f962
rinshad101 <rinshadkk1111@gmail.com>
2025-02-28 11:26:25 +0530

    new

--- leet-code-challenge/125-Valid_Palindrome.java
+++ leet-code-challenge/125-Valid_Palindrome.java
@@ -21,3 +21,24 @@
 // Explanation: s is an empty string "" after removing non-alphanumeric characters.
 // Since an empty string reads the same forward and backward, it is a palindrome.
 
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+        return true;
+    }
+}
+
