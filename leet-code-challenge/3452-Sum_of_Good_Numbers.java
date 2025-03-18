164f8d785819d7b1fde279f0c6b8bb166948f99e
rinshad101 <rinshadkk1111@gmail.com>
2025-03-18 09:55:04 +0530

    new

--- leet-code-challenge/3452-Sum_of_Good_Numbers.java
+++ leet-code-challenge/3452-Sum_of_Good_Numbers.java
@@ -1,3 +1,30 @@
+// Given an array of integers nums and an integer k, an element nums[i] is considered good if it is strictly greater than the elements at indices i - k and i + k (if those indices exist). If neither of these indices exists, nums[i] is still considered good.
+
+// Return the sum of all the good elements in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,2,1,5,4], k = 2
+
+// Output: 12
+
+// Explanation:
+
+// The good numbers are nums[1] = 3, nums[4] = 5, and nums[5] = 4 because they are strictly greater than the numbers at indices i - k and i + k.
+
+// Example 2:
+
+// Input: nums = [2,1], k = 1
+
+// Output: 2
+
+// Explanation:
+
+// The only good number is nums[0] = 2 because it is strictly greater than nums[1].
+
+
 class Solution {
     public int sumOfGoodNumbers(int[] nums, int k) {
         int goodSum = 0;
