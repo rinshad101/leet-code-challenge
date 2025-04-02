7dce3d2643b2d66c81c4893a5b8255f3e5817d99
rinshad101 <rinshadkk1111@gmail.com>
2025-04-02 16:41:51 +0530

    new

--- leet-code-challenge/12-Roman_to_integer.java
+++ leet-code-challenge/12-Roman_to_integer.java
@@ -52,3 +52,27 @@
 //  900 = CM
 //   90 = XC
 //    4 = IV
+
+class Solution {
+    public static String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder roman = new StringBuilder();
+        
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman.append(symbols[i]);
+            }
+        }
+        
+        return roman.toString();
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(intToRoman(3749)); 
+        System.out.println(intToRoman(58));
+        System.out.println(intToRoman(1994)); 
+    }
+}
