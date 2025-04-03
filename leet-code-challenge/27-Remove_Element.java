258519336e2f69a10674ad6818633a7634ff923e
rinshad101 <rinshadkk1111@gmail.com>
2025-04-03 20:36:11 +0530

    new

--- leet-code-challenge/27-Remove_Element.java
+++ leet-code-challenge/27-Remove_Element.java
@@ -38,3 +38,30 @@
 // Note that the five elements can be returned in any order.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; 
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {3,2,2,3};
+        int val1 = 3;
+        int k1 = solution.removeElement(nums1, val1);
+        System.out.println(k1);
+        
+        int[] nums2 = {0,1,2,2,3,0,4,2};
+        int val2 = 2;
+        int k2 = solution.removeElement(nums2, val2);
+        System.out.println(k2);
+    }
+}
