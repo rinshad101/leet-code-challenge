57d176e139ff6cda54c26a2634434217a6f3ae30
rinshad101 <rinshadkk1111@gmail.com>
2025-04-04 16:54:08 +0530

    new

--- leet-code-challenge/29-Divide_Two_Integers.java
+++ leet-code-challenge/29-Divide_Two_Integers.java
@@ -18,3 +18,35 @@
 // Input: dividend = 7, divisor = -3
 // Output: -2
 // Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        
+
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+        
+
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+        
+        int quotient = 0;
+        
+        
+        while (absDividend >= absDivisor) {
+            long tempDivisor = absDivisor, multiple = 1;
+            
+            while (absDividend >= (tempDivisor << 1)) {
+                tempDivisor <<= 1;
+                multiple <<= 1;
+            }
+            
+            absDividend -= tempDivisor;
+            quotient += multiple;
+        }
+        
+        return negative ? -quotient : quotient;
+    }
+}
