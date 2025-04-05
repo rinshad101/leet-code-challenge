1a8d2626539359d0407ad6e2ff4106e6f725c05d
rinshad101 <rinshadkk1111@gmail.com>
2025-04-05 09:38:21 +0530

    new

--- leet-code-challenge/263-Ugly_Number.java
+++ leet-code-challenge/263-Ugly_Number.java
@@ -19,3 +19,25 @@
 // Input: n = 14
 // Output: false
 // Explanation: 14 is not ugly since it includes the prime factor 7.
+
+class Solution {
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+
+        int[] divisors = {2, 3, 5};
+        for (int d : divisors) {
+            while (n % d == 0) {
+                n /= d;
+            }
+        }
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.isUgly(6));
+        System.out.println(sol.isUgly(1));
+        System.out.println(sol.isUgly(14));
+    }
+}
