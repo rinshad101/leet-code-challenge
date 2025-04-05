25d664d765ed67bd543d6d531b7f539d71eb33fb
rinshad101 <rinshadkk1111@gmail.com>
2025-04-05 09:48:11 +0530

    new

--- leet-code-challenge/268-Missing_Number.java
+++ leet-code-challenge/268-Missing_Number.java
@@ -31,3 +31,24 @@
 // Explanation:
 
 // n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.missingNumber(new int[]{3, 0, 1}));          
+        System.out.println(sol.missingNumber(new int[]{0, 1}));            
+        System.out.println(sol.missingNumber(new int[]{9,6,4,2,3,5,7,0,1})); 
+    }
+}
