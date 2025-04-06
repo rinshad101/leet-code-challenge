2925157a46e4e429fabd10f3a0cbd6b6e2b0c69e
rinshad101 <rinshadkk1111@gmail.com>
2025-04-06 15:41:36 +0530

    new

--- leet-code-challenge/41-First_Missing_Positive.java
+++ leet-code-challenge/41-First_Missing_Positive.java
@@ -20,3 +20,36 @@
 // Output: 1
 // Explanation: The smallest positive integer 1 is missing.
 
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int correctIndex = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums1 = {1, 2, 0};
+        int[] nums2 = {3, 4, -1, 1};
+        int[] nums3 = {7, 8, 9, 11, 12};
+
+        System.out.println(sol.firstMissingPositive(nums1));
+        System.out.println(sol.firstMissingPositive(nums2));
+        System.out.println(sol.firstMissingPositive(nums3));
+    }
+}
