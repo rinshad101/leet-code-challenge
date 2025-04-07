060858cd2ecc992e45519a124cf69196410e0d29
rinshad101 <rinshadkk1111@gmail.com>
2025-04-07 13:15:21 +0530

    new

--- leet-code-challenge/121-Best_Time_to_Buy_and_Sell_Stock.java
+++ leet-code-challenge/121-Best_Time_to_Buy_and_Sell_Stock.java
@@ -18,3 +18,19 @@
 // Output: 0
 // Explanation: In this case, no transactions are done and the max profit = 0.
 
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; 
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
