57c3e7468b4ce77197b9100d954dffc403cb183e
rinshad101 <rinshadkk1111@gmail.com>
2025-04-08 10:57:29 +0530

    new

--- leet-code-challenge/66-Plus_One.java
+++ leet-code-challenge/66-Plus_One.java
@@ -26,3 +26,22 @@
 // Incrementing by one gives 9 + 1 = 10.
 // Thus, the result should be [1,0].
 
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+}
+
+    
