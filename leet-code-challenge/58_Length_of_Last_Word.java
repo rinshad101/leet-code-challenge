a8bab6f3d96552c4b55e91e8accbfb3f0e316710
rinshad101 <rinshadkk1111@gmail.com>
2025-04-09 10:00:41 +0530

    new

--- leet-code-challenge/58_Length_of_Last_Word.java
+++ leet-code-challenge/58_Length_of_Last_Word.java
@@ -20,3 +20,17 @@
 // Output: 6
 // Explanation: The last word is "joyboy" with length 6.
 
+class Solution {
+    public static int lengthOfLastWord(String s) {
+        s = s.trim(); 
+
+        int lastSpace = s.lastIndexOf(' ');
+        return s.length() - lastSpace - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World")); 
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  ")); 
+        System.out.println(lengthOfLastWord("luffy is still joyboy"));
+    }
+}
