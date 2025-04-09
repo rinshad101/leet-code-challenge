57d6cfc880bdd1207ab8f8aa77d06d9b3c08e410
rinshad101 <rinshadkk1111@gmail.com>
2025-04-09 09:41:38 +0530

    new

--- leet-code-challenge/65-Valid_Number.java
+++ leet-code-challenge/65-Valid_Number.java
@@ -37,3 +37,60 @@
 
 // Output: false
 
+class Solution {
+    public static boolean isNumber(String s) {
+        s = s.trim();
+        boolean numSeen = false;
+        boolean dotSeen = false;
+        boolean eSeen = false;
+        boolean numAfterE = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                numSeen = true;
+                if (eSeen) numAfterE = true;
+            } else if (c == '+' || c == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            } else if (c == '.') {
+                if (dotSeen || eSeen) {
+                    return false;
+                }
+                dotSeen = true;
+            } else if (c == 'e' || c == 'E') {
+                if (eSeen || !numSeen) {
+                    return false;
+                }
+                eSeen = true;
+                numAfterE = false;
+            } else {
+                return false; 
+            }
+        }
+
+        return numSeen && numAfterE;
+    }
+
+    public static void main(String[] args) {
+        String[] validNumbers = {
+            "2", "0089", "-0.1", "+3.14", "4.", "-.9",
+            "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"
+        };
+        String[] invalidNumbers = {
+            "abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53", ".", "e"
+        };
+
+        System.out.println("Valid numbers:");
+        for (String s : validNumbers) {
+            System.out.println(s + ": " + isNumber(s));
+        }
+
+        System.out.println("\nInvalid numbers:");
+        for (String s : invalidNumbers) {
+            System.out.println(s + ": " + isNumber(s));
+        }
+    }
+}
