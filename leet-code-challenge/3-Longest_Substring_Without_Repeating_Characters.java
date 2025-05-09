75e6918a7db2170ef38dfdc48be512e076edb8c0
rinshad101 <rinshadkk1111@gmail.com>
2025-05-09 11:46:33 +0530

    new

--- leet-code-challenge/3-Longest_Substring_Without_Repeating_Characters.java
+++ leet-code-challenge/3-Longest_Substring_Without_Repeating_Characters.java
@@ -19,3 +19,28 @@
 // Explanation: The answer is "wke", with the length of 3.
 // Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        if (s.length() == 1) return 1;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLength = 0; 
+        int left = 0; 
+        
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            
+            if (map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1;
+            }
+            
+            map.put(c, right);
+            
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
+    
+}
